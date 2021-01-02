ecc7a31637b655fc7f9f9bc563ecba5d273d1309
Ruby Learner <newwaiphyo33@gmail.com>
2021-01-02 21:56:35 +0630

    interface add

--- lib/data/contact_repositorys.dart
+++ lib/data/contact_repositorys.dart
@@ -6,6 +6,7 @@ import 'model/contact.dart';
 abstract class ContactRepository{
   //view and bloc layer can work with interface
   //and delegate real implementation
+  //and inject with impl
   Future<List<Contact>> getContact();
 
   Future<Contact> addContact(Contact contact);
